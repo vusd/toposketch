2ee28c7c35ec6ddb153829068313c6ab7691f264
Ian Loh <ianlohjy@hotmail.com>
2017-08-10 11:53:19 +1200

    Removed A/B script test. Moved Piwik to its js file.

---
+++ js/piwik/piwik.js
@@ -0,0 +1,21 @@
+
+var _paq = _paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+
+(
+    function() 
+    {
+        var u="//toposketch.innocraft.cloud/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    }
+)();
+
+function log_event(category, action, name, value)
+{
+    _paq.push(['trackEvent', category, action, name, value]);
+}
